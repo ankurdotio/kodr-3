b35cedf58c234790c1a1d71c0fa8bb2b1949b354
ankurdotio <prajapatiankur8349@gmail.com>
2026-05-30 12:40:41 +0530

    chore: sync node_modules dependencies and project artifacts

--- v0/sandbox/agent/src/app.js
+++ v0/sandbox/agent/src/app.js
@@ -8,6 +8,16 @@ const WORKDIR = "/workspace"
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
